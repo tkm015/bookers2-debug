c0841f027dd274cbf820b0f82928f920b591115c
tkm015 <takumi200015@yahoo.co.jp>
2020-06-23 21:28:59 +0900

    up

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,19 @@
 class BookCommentsController < ApplicationController
 	def create
-		@book = Book.find(params[:book_id])
-		@comment = BookComment.new(book_comment_params)
-		@comment.book_id = @book.id
-		@comment.user_id = current_user.id
-		@comment.save
-	end
+    @book = Book.find(params[:book_id])
+    @book_new = Book.new
+    @book_comment = @book.book_comments.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.save
 
-	def destroy
-		@comment = BookComment.find(params[:id])
-		@comment.destroy
-	end
+  end
 
+  def destroy
+    @book_comment = BookComment.find(params[:book_id])
+    @book = @book_comment.book
+    @book_comment.destroy
+
+  end
 	private
 	def book_comment_params
 		params.require(:book_comment).permit(:comment)
